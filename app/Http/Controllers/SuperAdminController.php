fde6be4db1ef7ab4a48307fd42e0d7047fab1a83
loloy03 <jamesloupoli03@gmail.com>
2023-10-19 21:05:33 +0800

    add fname and lname in table and creation of acc

--- app/Http/Controllers/SuperAdminController.php
+++ app/Http/Controllers/SuperAdminController.php
@@ -23,14 +23,16 @@ class SuperAdminController extends Controller
     public function register_account(Request $request)
     {
         $request->validate([
-            'name' => ['required', 'string', 'max:255'],
+            'first_name' => ['required', 'string', 'max:255'],
+            'last_name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:' . User::class],
             'user_type' => ['required', 'string', 'max:255'],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
         ]);
 
         $user = User::create([
-            'name' => $request->name,
+            'first_name' => $request->first_name,
+            'last_name' => $request->last_name,
             'email' => $request->email,
             'user_type' => $request->user_type,
             'password' => Hash::make($request->password),
