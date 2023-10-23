160c7ae0e594ae29ef6353439b3bf0a8082bd117
loloy03 <jamesloupoli03@gmail.com>
2023-10-23 19:22:19 +0800

    fix UI in view appointments

--- resources/views/admin/view-list-appointment-page.blade.php
+++ resources/views/admin/view-list-appointment-page.blade.php
@@ -8,34 +8,39 @@
 
 
     @foreach ($appointments as $appointment)
-    <div class="py-5">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
 
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <a href="{{ route('show-appointmet', ['id' => $appointment->appointment_id]) }}" class="block py-5">
-                    <div class="p-6 text-gray-900">
-                        <div>
-                            Appointment ID: {{ $appointment->appointment_id }}
+    <div class="flex justify-center items-center">
+        <div class="w-full m-6 bg-white shadow-md overflow-hidden rounded-lg hover:bg-red-100 transition-all duration-300 ease-in-out">
+            <a href="{{ route('show-appointmet', ['id' => $appointment->appointment_id]) }}" class="block py-5">
+                <div class="p-6 text-gray-900 ">
+                    <div class="md:flex flex-row ">
+                        <div class="basis-1/2 flex justify-center items-center text-6xl mb-5">
+                            <i class=" fa-regular fa-calendar-check"></i>
                         </div>
-                        <div>
-                            Title: {{ $appointment->appointment_title }}
+                        <div class="basis-1/2">
+                            <div class="font-bold text-2xl">
+                                Title: {{ $appointment->appointment_title }}
+                            </div>
+                            <div>
+                                Appointment ID: {{ $appointment->appointment_id }}
+                            </div>
                         </div>
-                        <div>
-                            Description: {{ $appointment->appointment_description }}
-                        </div>
-                        <div>
-                            Assigned Doctor: Dr. {{ $appointment->first_name }} {{ $appointment->last_name }}
-                        </div>
-                        <div>
-                            Start Date: {{ $appointment->appointment_start_date }}
-                        </div>
-                        <div>
-                            End Date: {{ $appointment->appointment_end_date }}
+                        <div class="basis-1/2">
+                            <div>
+                                Assigned Doctor: Dr. {{ $appointment->first_name }} {{ $appointment->last_name }}
+                            </div>
+                            <div>
+                                Start Date: {{ $appointment->appointment_start_date }}
+                            </div>
+                            <div>
+                                End Date: {{ $appointment->appointment_end_date }}
+                            </div>
                         </div>
                     </div>
-                </a>
-            </div>
+                </div>
+            </a>
         </div>
     </div>
+
     @endforeach
 </x-app-layout>
