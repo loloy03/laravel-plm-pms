a68a81d6ca229bd2cb0feed6f75f34f6cd93a280
loloy03 <jamesloupoli03@gmail.com>
2023-11-02 16:19:27 +0800

    fix declined appointment mesage

--- resources/views/emails/declined_appointment_sched.blade.php
+++ resources/views/emails/declined_appointment_sched.blade.php
@@ -10,7 +10,7 @@
 <body>
     <h3>Good Day Mr/Ms. {{ ucfirst($mail_data['last_name']) }}</h3>
     <p>We regret to inform you that your appointment request (ID: {{$mail_data['appointment_request_id']}}) has been declined.</p>
-    <p>Unfortunately, we are unable to accommodate your requested appointment at this time.</p>
+    <p>Unfortunately, we are unable to accommodate your requested appointment at this time, but you can reschedule your appointment if there is an availability.</p>
     <p>Thank you for understanding.</p>
 </body>
 
