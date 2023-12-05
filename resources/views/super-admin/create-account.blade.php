618eff3a4c74e909aa968224ad16432557fe9c8d
loloy03 <jamesloupoli03@gmail.com>
2023-12-05 14:15:13 +0800

    update the display of creation of account

--- resources/views/super-admin/create-account.blade.php
+++ resources/views/super-admin/create-account.blade.php
@@ -4,73 +4,74 @@
             {{ __('Create an Account') }}
         </h2>
     </x-slot>
-    <div class="flex justify-center items-center">
-        <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
-            <form method="POST" action="{{ route('register-account') }}">
-                @csrf
+    <div class="m-6 bg-white shadow-md overflow-hidden rounded-lg">
+        <form method="POST" action="{{ route('register-account') }}">
+            @if (Session::has('success'))
+            <x-success :message="Session::get('success')" />
+            @endif
+            @csrf
+            <div class="md:flex flex-row">
+                <div class="basis-1/2 md:p-10 p-5">
+                    <!-- For first name -->
+                    <div class="mt-4">
+                        <x-input-label for="first_name" :value="__('First Name')" />
+                        <x-text-input id="f_name" class="block mt-1 w-full" type="text" name="first_name" :value="old('name')" required autofocus autocomplete="first_name" />
+                        <x-input-error :messages="$errors->get('first_name')" class="mt-2" />
+                    </div>
 
-                @if (Session::has('success'))
-                <x-success :message="Session::get('success')" />
-                @endif
+                    <!-- For last name-->
+                    <div class="mt-4">
+                        <x-input-label for="last_name" :value="__('Last Name')" />
+                        <x-text-input id="last_name" class="block mt-1 w-full" type="text" name="last_name" :value="old('name')" required autofocus autocomplete="last_name" />
+                        <x-input-error :messages="$errors->get('last_name')" class="mt-2" />
+                    </div>
 
-                <!-- For first name -->
-                <div class="mt-4">
-                    <x-input-label for="first_name" :value="__('First Name')" />
-                    <x-text-input id="f_name" class="block mt-1 w-full" type="text" name="first_name" :value="old('name')" required autofocus autocomplete="first_name" />
-                    <x-input-error :messages="$errors->get('first_name')" class="mt-2" />
+                    <!-- Email Address -->
+                    <div class="mt-4">
+                        <x-input-label for="email" :value="__('Email')" />
+                        <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" />
+                        <x-input-error :messages="$errors->get('email')" class="mt-2" />
+                    </div>
                 </div>
 
-                <!-- For last name-->
-                <div class="mt-4">
-                    <x-input-label for="last_name" :value="__('Last Name')" />
-                    <x-text-input id="last_name" class="block mt-1 w-full" type="text" name="last_name" :value="old('name')" required autofocus autocomplete="last_name" />
-                    <x-input-error :messages="$errors->get('last_name')" class="mt-2" />
+                <div class="basis-1/2 md:p-10 px-5">
+                    <!-- Create Account as -->
+                    <div class="mt-4">
+                        <x-input-label for="user_type" :value="__('Create Account as')" />
+                        <select name="user_type" class="block mt-1 w-full border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm">
+                            <option class="mt-2" disabled selected>Select User Type</option>
+                            <option class="mt-2" value="super-admin">Super Admin</option>
+                            <option class="mt-2" value="admin">Admin</option>
+                            <option class="mt-2" value="doctor">Doctor</option>
+                            <option class="mt-2" value="student">Student</option>
+                        </select>
+                        <x-input-error :messages="$errors->get('user_type')" class="mt-2" />
+                    </div>
+
+                    <!-- Password -->
+                    <div class="mt-4">
+                        <x-input-label for="password" :value="__('Password')" />
+
+                        <x-text-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="new-password" />
+
+                        <x-input-error :messages="$errors->get('password')" class="mt-2" />
+                    </div>
+
+                    <!-- Confirm Password -->
+                    <div class="mt-4">
+                        <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
+
+                        <x-text-input id="password_confirmation" class="block mt-1 w-full" type="password" name="password_confirmation" required autocomplete="new-password" />
+
+                        <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
+                    </div>
                 </div>
-
-                <!-- Email Address -->
-                <div class="mt-4">
-                    <x-input-label for="email" :value="__('Email')" />
-                    <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" />
-                    <x-input-error :messages="$errors->get('email')" class="mt-2" />
-                </div>
-
-                <!-- Create Account as -->
-                <div class="mt-4">
-                    <x-input-label for="user_type" :value="__('Create Account as')" />
-                    <select name="user_type" class="block mt-1 w-full border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm">
-                        <option class="mt-2" disabled selected >Select User Type</option>
-                        <option class="mt-2" value="super-admin">Super Admin</option>
-                        <option class="mt-2" value="admin">Admin</option>
-                        <option class="mt-2" value="doctor">Doctor</option>
-                        <option class="mt-2" value="student">Student</option>
-                    </select>
-                    <x-input-error :messages="$errors->get('user_type')" class="mt-2" />
-                </div>
-
-                <!-- Password -->
-                <div class="mt-4">
-                    <x-input-label for="password" :value="__('Password')" />
-
-                    <x-text-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="new-password" />
-
-                    <x-input-error :messages="$errors->get('password')" class="mt-2" />
-                </div>
-
-                <!-- Confirm Password -->
-                <div class="mt-4">
-                    <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
-
-                    <x-text-input id="password_confirmation" class="block mt-1 w-full" type="password" name="password_confirmation" required autocomplete="new-password" />
-
-                    <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
-                </div>
-
-                <div class="flex items-center justify-end mt-4">
-                    <x-primary-button class="ml-4">
-                        {{ __('Register') }}
-                    </x-primary-button>
-                </div>
-            </form>
-        </div>
+            </div>
+            <div class="flex items-center justify-center m-4">
+                <x-primary-button>
+                    {{ __('Register') }}
+                </x-primary-button>
+            </div>
+        </form>
     </div>
 </x-app-layout>
