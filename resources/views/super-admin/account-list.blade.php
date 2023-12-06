2a0172e37fde7e47999c5aad645997f29f180d9f
loloy03 <jamesloupoli03@gmail.com>
2023-12-06 13:59:40 +0800

    fixi UI in account list

--- resources/views/super-admin/account-list.blade.php
+++ resources/views/super-admin/account-list.blade.php
@@ -16,15 +16,28 @@
     </div>
     @foreach($accounts as $account)
     <div class="flex justify-center items-center ">
-        <div class="w-full mx-5 md:mx-10 my-2 bg-white shadow-md overflow-hidden rounded-sm text-sm flex flex-row p-5 rounded-md rounded-lg">
+        <div class="w-full mx-5 md:mx-10 my-2 bg-white overflow-hidden rounded-sm text-sm flex flex-row p-5 rounded-md rounded-lg">
             <div class="basis-3/4">
-                <div> Account ID: {{ $account->id }}</div>
-                <div class="text-base font-bold"> Name: {{ ucfirst($account->first_name) }}
-                    {{ ucfirst($account->last_name) }}
+                <div class="text-xs flex">
+                    <div> Account ID:</div>
+                    <div class="ml-1 font-bold"> {{ $account->id }}</div>
                 </div>
-                <div>Created Date: {{ $account->created_at->format('F d, Y H:i:s') }}</div>
-
-                <div>User Type: {{ strtoupper($account->user_type) }}</div>
+                <div class="text-xs">Created Date: {{ $account->created_at->format('F d, Y H:i:s') }}</div>
+                <div class="flex">
+                    <div>
+                        Name:
+                    </div>
+                    <div class="ml-1 font-bold">
+                        {{ ucfirst($account->first_name) }}
+                    </div>
+                    <div class="ml-1 font-bold">
+                        {{ ucfirst($account->last_name) }}
+                    </div>
+                </div>
+                <div class="flex">
+                    <div>User Type:</div>
+                    <div class="font-bold ml-1"> {{ strtoupper($account->user_type) }}</div>
+                </div>
             </div>
             <div class="basis-1/4 flex justify-center items-center">
                 <form action="{{ route('account-show', $account->id) }}" method="get">
