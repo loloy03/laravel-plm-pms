ba60fe184df7086bc7e94baca0dae63e01a58581
JuvinIvan <119097416+JuvinIvan@users.noreply.github.com>
2023-12-27 18:08:34 +0800

    Add files via upload

---
+++ resources/views/student/appointmentspage.blade.php
@@ -0,0 +1,70 @@
+<x-app-layout>
+
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('View Available Appointments') }}
+        </h2>
+    </x-slot>
+
+    <div class="mt-10"></div>
+    @csrf
+
+    @if (Session::has('success'))
+        <x-success :message="Session::get('success')" />
+    @elseif(Session::has('warning'))
+        <x-success :message="Session::get('warning')" />
+    @endif
+    @foreach ($appointments as $appointment)
+
+    <div class="flex justify-center items-center">
+        <div class="w-full mx-10 my-2 bg-white shadow-md overflow-hidden rounded-lg hover:bg-red-100 transition-all duration-300 ease-in-out">
+            <a href="{{ route('availappointments', ['id' => $appointment->appointment_id]) }}" class="block px-10 py-5">
+                <div class="p-3 text-gray-900 ">
+                    <div class="md:flex flex-row ">
+                        <div class="basis-1/2 flex justify-center items-center text-6xl xs:mb-5 sm:mb-0">
+                            <i class=" fa-regular fa-calendar-check"></i>
+                        </div>
+                        <div class="basis-1/2">
+                            <div class="font-bold text-3xl">
+                                {{ $appointment->appointment_title }}
+                            </div>
+                            <div class="text-sm mb-4 ">
+                                Appointment ID: {{ $appointment->appointment_id }}
+                            </div>
+                        </div>
+                        <div class="basis-1/2">
+                            <!--changing the time format on the view--->
+                            <div class="flex">
+                                <div class="font-bold">
+                                    Start Date: &nbsp;
+                                </div>
+                                <div>
+                                    {{ \Carbon\Carbon::parse($appointment->appointment_start_date)->format('F d, Y, h:i A') }}
+                                </div>
+
+                            </div>
+                            <div class="flex">
+                                <div class="font-bold">
+                                    End Date: &nbsp;
+                                </div>
+                                <div>
+                                    {{ \Carbon\Carbon::parse($appointment->appointment_end_date)->format('F d, Y, h:i A') }}
+                                </div>
+                            </div>
+                            <div class="flex">
+                                <div class="font-bold">
+                                    Assigned Doctor: &nbsp;
+                                </div>
+                                <div>
+                                    Dr. {{ $appointment->first_name }} {{ $appointment->last_name }}
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </a>
+        </div>
+    </div>
+
+    @endforeach
+</x-app-layout>
