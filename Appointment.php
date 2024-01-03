5693b4a69a160c7d6dc5014671c841cec2200c2b
JuvinIvan <119097416+JuvinIvan@users.noreply.github.com>
2024-01-04 02:58:49 +0800

    Add files via upload

---
+++ Appointment.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Appointment extends Model
+{
+    use HasFactory;
+    protected $primaryKey = 'appointment_id';
+    protected $fillable = [
+        'appointment_title',
+        'appointment_description',
+        'appointment_start_date',
+        'appointment_end_date',
+        'appointment_allowed_patients',
+        'appointment_assigned_doctor_id'
+    ];
+    
+    public function assignedDoctor()
+    {
+        return $this->belongsTo(User::class, 'appointment_assigned_doctor_id');
+    }
+}
---
+++ AppointmentRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class AppointmentRequest extends Model
+{
+    use HasFactory;
+
+    protected $primaryKey = 'appointment_request_id';
+    protected $fillable = [
+        'status',
+        'first_name',
+        'last_name',
+        'user_id',
+        'appointment_id',
+    ];
+
+    public $timestamps = false;  // Disable automatic timestamps
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function appointment()
+    {
+        return $this->belongsTo(Appointment::class, 'appointment_id');
+    }
+}
