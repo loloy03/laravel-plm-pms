f53883ee6eec5cbc6a40b106371da5e1c7b84f85
loloy03 <jamesloupoli03@gmail.com>
2024-01-05 12:36:06 +0800

    fix merged conflicts

--- routes/web.php
+++ routes/web.php
@@ -43,6 +43,9 @@ route::get('/view-list-appointment-page', [AdminController::class, 'view_list_ap
 Route::get('/filter-appointments', [AdminController::class, 'view_list_appointment'])->middleware(['auth', 'admin'])->name('filter-appointments');
 Route::get('/appointment/{id}', [AdminController::class, 'show_appointment'])->middleware(['auth', 'admin'])->name('show-appointmet');
 Route::get('/show_user_info/{id}', [AdminController::class, 'show_patient_info'])->middleware(['auth', 'admin'])->name('show_patient_info');
+Route::get('/appointment/show-edit/{id}', [AdminController::class, 'appointment_show_edit'])->middleware(['auth', 'admin'])->name('appointment-show-edit');
+Route::post('/appointment/edit/{id}', [AdminController::class, 'edit_appointment'])->middleware(['auth', 'admin'])->name('edit-appointment');
+Route::post('/appointment/delete/{id}', [AdminController::class, 'appointment_delete'])->middleware(['auth', 'admin'])->name('appointment-delete');
 Route::get('/appointments/{id}/download-pdf', [AdminController::class, 'downloadPDF'])->name('download_pdf');
 
 //for doctor only
